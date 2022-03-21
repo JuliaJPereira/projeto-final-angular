994a1efe18814fd35fa9a4f2dc3a82d049fbd1fd
Julia Pereira <juliapereira.odc@gmail.com>
2022-03-21 14:49:35 -0300

    configure component routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  /*{
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+
+  }*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
