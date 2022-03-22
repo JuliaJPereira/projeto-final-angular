e5df1244232e2f2b6681b06ece31a7ec362c8d25
Julia Pereira <juliapereira.odc@gmail.com>
2022-03-22 09:32:54 -0300

    create dashboard routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
-  /*{
+  {
     path: '',
     redirectTo: '/dashboard',
-    pathMatch: 'full'
-
-  }*/
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
